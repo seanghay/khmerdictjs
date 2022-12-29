9b83ee6401fb50a7468f61e8b28d59f89038369c
Seanghay Yath <seanghay.dev@gmail.com>
2022-12-30 00:13:03 +0700

    progress bar

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 import './main.css'
 import debounce from 'lodash.debounce'
+import ProgressBar from 'progressbar.js'
 
 const searchElement = document.querySelector('#search');
 const resultElement = document.querySelector('#result');
+const progressContainerElement = document.querySelector('#progress-container');
+
+const line = new ProgressBar.Line(progressContainerElement, {
+  color: 'white',
+  strokeWidth: 1
+})
 
 const worker = new Worker(new URL("./worker.js", import.meta.url), {
   type: "module"
 })
 
 
+
+
 worker.addEventListener('message', (msg) => {
+
+  if (typeof msg.data === 'object' && msg.data.progress) {
+    //progressElement.value = msg.data.progress;
+    line.animate(msg.data.progress / 100)
+    return;
+  }
+
   if (msg.data === "READY") {
     console.log('ready')
+
+    progressContainerElement.style.display = 'none';
     searchElement.disabled = false;
     return
   }
@@ -19,17 +37,16 @@ worker.addEventListener('message', (msg) => {
   const { data, time } = msg.data;
   const millis = Math.round((time * 10000) / 1000) / 10000;
 
-
   if (data.length === 0) {
     resultElement.innerHTML = 'Empty'
     return;
   }
 
-    resultElement.innerHTML = `<p>${millis} វិនាទី, រកឃើញ ${data.length} ពាក្យ</p>` + data.map((item) => {
-      return `<li><strong class="word">${item.subword || item.main || ""}<span class="pos">${item.part_of_speech || "មិនមាន"}</span></strong><p>${item.definition}</p> <p class="example">${item.example || ""}</p></li>`;
-    }).join('')
+  resultElement.innerHTML = `<p>${millis} វិនាទី, រកឃើញ ${data.length} ពាក្យ</p>` + data.map((item) => {
+    return `<li><strong class="word">${item.subword || item.main || ""}<span class="pos">${item.part_of_speech || "មិនមាន"}</span></strong><p>${item.definition}</p> <p class="example">${item.example || ""}</p></li>`;
+  }).join('')
 
-  })
+})
 
 searchElement.addEventListener('input', debounce(() => {
   const text = searchElement.value;
--- worker.js
+++ worker.js
@@ -5,10 +5,44 @@ import MiniSearch from 'minisearch'
 let _minisearch;
 let _words = [];
 
+async function downloadFileBlob(url, progress = () => { }) {
+  const response = await fetch(url);
+  const contentLength = response.headers.get('content-length');
+  const total = parseInt(contentLength, 10);
+  let loaded = 0;
+
+  const res = new Response(new ReadableStream({
+    async start(controller) {
+      const reader = response.body.getReader();
+      for (; ;) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        loaded += value.byteLength;
+        progress(loaded / total);
+        controller.enqueue(value);
+      }
+      controller.close();
+    },
+  }));
+
+  return res.blob();
+}
+
 async function main() {
+  let percent1 = 0;
+  let percent2 = 0;
+  let percent3 = 0;
+
+  const totalProgress = () => {
+    postMessage({ progress: percent1 + percent2 + percent3 });
+  }
+
   console.time('download');
-  const res = await fetch(dbUrl);
-  const blob = await res.blob();
+  const blob = await downloadFileBlob(dbUrl, progress => {
+    percent1 = (progress * 100 / 3);
+    totalProgress()
+  });
+
   console.timeEnd('download');
 
   const reader = new ZipReader(new BlobReader(blob))
@@ -16,7 +50,13 @@ async function main() {
 
   for (const entry of entries) {
     console.time('decompress')
-    const json = await entry.getData(new TextWriter())
+    const json = await entry.getData(new TextWriter(), {
+      onprogress: (loaded, total) => {
+        const progress = loaded / total;
+        percent2 = (progress * 100 / 3);
+        totalProgress()
+      }
+    })
     console.timeEnd('decompress')
     console.time('parse')
     const items = JSON.parse(json).map((item, id) => ({ ...item, id }));
@@ -26,6 +66,10 @@ async function main() {
     console.log(items.length);
 
     console.time('index')
+    
+    percent3 += 33.33 / 2;
+    totalProgress()
+
     const minisearch = new MiniSearch({
       fields: ['main', 'subword', 'definition', 'example'],
       searchOptions: {
@@ -37,7 +81,11 @@ async function main() {
     minisearch.addAll(items);
     console.timeEnd('index')
     _minisearch = minisearch;
+  
     postMessage("READY")
+    percent3 += 33.33 / 2;
+    totalProgress()
+  
   }
 }
 
