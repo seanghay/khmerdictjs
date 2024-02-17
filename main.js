9427b2800a97ac2ed7598b2152ae529b8db08101
Seanghay Yath <seanghay.dev@gmail.com>
2024-02-17 12:39:06 +0700

    add tts service

--- main.js
+++ main.js
@@ -3,6 +3,35 @@ import './main.css';
 import debounce from 'lodash.debounce';
 import ProgressBar from 'progressbar.js';
 
+/**
+ * @param {string} text 
+ */
+window.synthesize = async function (e, text) {
+  text = text.replace(/[^\u1780-\u17d2]+/, '')
+  if (!text) return;
+  let el = e.target;
+  while(!(el instanceof HTMLButtonElement)) {
+    el = el.parentElement;
+  }
+  el.disabled = true;
+  try {
+    const _AudioContext = window.AudioContext || window.webkitAudioContext;
+    const context = new _AudioContext();
+    const audioUrl = new URL("https://klea-js.fly.dev/" + text)
+    const response = await fetch(audioUrl);
+    const buffer = await response.arrayBuffer();
+    const audioBuffer = await context.decodeAudioData(buffer);
+    const source = context.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(context.destination);
+    source.start();
+  } catch (e) {
+    console.error(e);
+  } finally {
+  el.disabled = false;
+  }
+}
+
 const sampleListElement = document.querySelector('#sample-list');
 const searchElement = document.querySelector('#search');
 const resultElement = document.querySelector('#result');
@@ -124,8 +153,17 @@ worker.addEventListener('message', (msg) => {
         <p>${sanitizeDef(item.definition)}</p>
         ${el}
         ${noteEl}
-        <p class="pronunciation">អានថា៖ <span class="white">${item.pronunciation}</span></p>
+        <div class="flex items-center gap">        
+          <p class="pronunciation">អានថា៖ <span class="white">${item.pronunciation}</span></p>
+          <button onclick='synthesize(event, ${JSON.stringify(item.subword || item.main || "")})' class="button flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M14 20.725v-2.05q2.25-.65 3.625-2.5t1.375-4.2q0-2.35-1.375-4.2T14 5.275v-2.05q3.1.7 5.05 3.138T21 11.975q0 3.175-1.95 5.613T14 20.725M3 15V9h4l5-5v16l-5-5zm11 1V7.95q1.175.55 1.838 1.65T16.5 12q0 1.275-.663 2.363T14 16"/></svg>
+            <span>ស្ដាប់</span>
+          </button>
+        </div>
       </li>
+
+
+
     `;
   }).join('')
 
